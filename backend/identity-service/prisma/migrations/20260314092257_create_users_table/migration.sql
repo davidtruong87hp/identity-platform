8f1faecdd31c30b5ab23e1ba71e2368dc88eb322
davidtruong87hp <truongquangchien87@icloud.com>
2026-03-14 16:24:35 +0700

    feat: add User model with email verification and password reset fields

---
+++ backend/identity-service/prisma/migrations/20260314092257_create_users_table/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "emailVerifiedAt" TIMESTAMP(3),
+    "emailVerifyToken" TEXT,
+    "emailVerifyTokenExp" TIMESTAMP(3),
+    "passwordResetToken" TEXT,
+    "passwordResetTokenExp" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_emailVerifyToken_key" ON "users"("emailVerifyToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_passwordResetToken_key" ON "users"("passwordResetToken");
