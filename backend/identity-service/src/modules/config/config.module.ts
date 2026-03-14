53009cd587531df609c24657205ec4360e30e9c9
davidtruong87hp <truongquangchien87@icloud.com>
2026-03-14 14:54:16 +0700

    feat: add config module with env validation

---
+++ backend/identity-service/src/modules/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
+})
+export class AppConfigModule {}
